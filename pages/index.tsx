8f91a5308d18628b5cfdf5a15e3ede37a7fdf6ab
qnrjs42 <qnrjs42@naver.com>
2022-06-16 00:19:16 +0900

    #4-8. More Modifers

--- pages/index.tsx
+++ pages/index.tsx
@@ -83,22 +83,47 @@ import type { NextPage } from 'next';
 //   );
 // };
 
+// const Home: NextPage = () => {
+//   return (
+//     <form className='flex flex-col space-y-2 p-5'>
+//       <input
+//         type='text'
+//         required
+//         placeholder='Username'
+//         className='border p-1 peer border-gray-400 rounded-md'
+//       />
+//       <span className='hidden peer-invalid:block peer-invalid:text-red-500'>
+//         This input is invalid
+//       </span>
+//       <span className='hidden peer-valid:block peer-valid:text-teal-500'>Awesome Username</span>
+//       {/* <input type='password' required placeholder='Password' /> */}
+//       <input type='submit' value='Login' className='bg-white' />
+//     </form>
+//   );
+// };
+
 const Home: NextPage = () => {
   return (
-    <form className='flex flex-col space-y-2 p-5'>
-      <input
-        type='text'
-        required
-        placeholder='Username'
-        className='border p-1 peer border-gray-400 rounded-md'
-      />
-      <span className='hidden peer-invalid:block peer-invalid:text-red-500'>
-        This input is invalid
-      </span>
-      <span className='hidden peer-valid:block peer-valid:text-teal-500'>Awesome Username</span>
-      {/* <input type='password' required placeholder='Password' /> */}
-      <input type='submit' value='Login' className='bg-white' />
-    </form>
+    <div className='flex flex-col space-y-2 p-5'>
+      {/* <details className='select-none open:text-white open:bg-indigo-400'>
+        <summary className='cursor-pointer'>What is my fav.food.</summary>
+        <span>김치찌개</span>
+      </details> */}
+      {/* <ul className='list-decimal marker:text-teal-500'>
+        <li>hi</li>
+        <li>hi</li>
+        <li>hi</li>
+      </ul> */}
+      {/* <input
+        type='file'
+        className='file:cursor-pointer file:hover:text-purple-400 file:hover:bg-white file:hover:border-purple-400 file:hover:border file:transition-colors file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-400'
+      /> */}
+      <p className='first-letter:text-7xl first-letter:hover:text-purple-400'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolorem unde molestiae
+        assumenda, quo eum reprehenderit ab id. Quod vero repellendus voluptatem dolorum perferendis
+        neque accusantium magni eum, similique aperiam?
+      </p>
+    </div>
   );
 };
 
