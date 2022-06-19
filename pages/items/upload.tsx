ef241b01e614c3e619ae3fe929c4697a1daaa68e
qnrjs42 <qnrjs42@naver.com>
2022-06-19 14:11:57 +0900

    #5-15. Add Stream

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 
 const Upload: NextPage = () => {
   return (
-    <div className='px-4 py-16'>
+    <div className='px-4 space-y-5 py-10'>
       <div>
         <label className='w-full text-gray-600 cursor-pointer hover:border-orange-500 hover:text-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md'>
           <svg
@@ -23,7 +23,20 @@ const Upload: NextPage = () => {
           <input className='hidden' type='file' />
         </label>
       </div>
-      <div className='my-5'>
+      <div>
+        <label className='mb-1 block text-sm font-medium text-gray-700' htmlFor='name'>
+          Name
+        </label>
+        <div className='rounded-md relative flex  items-center shadow-sm'>
+          <input
+            id='name'
+            type='email'
+            className='appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500'
+            required
+          />
+        </div>
+      </div>
+      <div>
         <label className='mb-1 block text-sm font-medium text-gray-700' htmlFor='price'>
           Price
         </label>
@@ -43,15 +56,20 @@ const Upload: NextPage = () => {
         </div>
       </div>
       <div>
-        <label className='mb-1 block text-sm font-medium text-gray-700'>Description</label>
+        <label htmlFor='description' className='mb-1 block text-sm font-medium text-gray-700'>
+          Description
+        </label>
         <div>
           <textarea
+            id='description'
             className='mt-1shadow-sm w-full focus:ring-2 focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500'
             rows={4}
           />
         </div>
       </div>
-      <button>Upload product</button>
+      <button className='w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none'>
+        Upload item
+      </button>
     </div>
   );
 };
