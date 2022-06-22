7befc60524f131ffca56a004ca8db1ca53c0c0a3
qnrjs42 <qnrjs42@naver.com>
2022-06-22 16:06:54 +0900

    #7-3. Validation

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -3,15 +3,27 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 const FormsPage: NextPage = () => {
-  const { register, watch } = useForm();
+  const { register, handleSubmit } = useForm();
 
-  console.log(watch());
+  const onValid = () => {
+    console.log('im valid bby');
+  };
 
   return (
-    <form>
-      <input {...register('username')} required type='text' placeholder='Username' />
-      <input {...register('email')} required type='email' placeholder='Email' />
-      <input {...register('password')} required type='password' placeholder='Password' />
+    <form onSubmit={handleSubmit(onValid)}>
+      <input
+        {...register('username', { required: true })}
+        required
+        type='text'
+        placeholder='Username'
+      />
+      <input {...register('email', { required: true })} required type='email' placeholder='Email' />
+      <input
+        {...register('password', { required: true })}
+        required
+        type='password'
+        placeholder='Password'
+      />
       <input type='submit' value='Create Account' />
     </form>
   );
