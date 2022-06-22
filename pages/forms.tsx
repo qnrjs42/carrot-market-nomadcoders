041f23da53db89c57316e081a1939c6aec6d3285
qnrjs42 <qnrjs42@naver.com>
2022-06-22 16:16:55 +0900

    #7-4. Validation part Two

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,26 +1,43 @@
 import { NextPage } from 'next';
 import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { FieldErrors, useForm } from 'react-hook-form';
+
+interface LoginForm {
+  username: string;
+  email: string;
+  password: string;
+}
 
 const FormsPage: NextPage = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm<LoginForm>();
 
-  const onValid = () => {
-    console.log('im valid bby');
+  const onValid = (data: LoginForm) => {
+    console.log('im valid bby', data);
+  };
+  const onInValid = (errors: FieldErrors) => {
+    console.log(errors);
   };
 
   return (
-    <form onSubmit={handleSubmit(onValid)}>
+    <form onSubmit={handleSubmit(onValid, onInValid)}>
       <input
-        {...register('username', { required: true })}
-        required
+        {...register('username', {
+          required: 'Username is Required',
+          minLength: {
+            message: 'The username should be 5',
+            value: 5,
+          },
+        })}
         type='text'
         placeholder='Username'
       />
-      <input {...register('email', { required: true })} required type='email' placeholder='Email' />
       <input
-        {...register('password', { required: true })}
-        required
+        {...register('email', { required: 'Email is Required' })}
+        type='email'
+        placeholder='Email'
+      />
+      <input
+        {...register('password', { required: 'Password is Required' })}
         type='password'
         placeholder='Password'
       />
