7e3843deec10d4b3d956994886b8fc2de6bb075c
qnrjs42 <qnrjs42@naver.com>
2022-09-15 21:24:15 +0900

    #10-4. SWR

--- libs/client/useUser.tsx
+++ libs/client/useUser.tsx
@@ -1,19 +1,12 @@
+import useSWR from 'swr';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+
+const fetcher = (url: string) => fetch(url).then(response => response.json());
 
 export default function useUser() {
-  const [user, setUser] = useState();
-  const router = useRouter();
+  const { data, error } = useSWR('/api/users/me', fetcher);
 
-  useEffect(() => {
-    fetch('/api/users/me')
-      .then(response => response.json())
-      .then(data => {
-        if (!data.ok) {
-          return router.replace('/enter');
-        }
+  // const router = useRouter();
 
-        setUser(data.profile);
-      });
-  }, [router]);
+  return data;
 }
