673b840c3bedcebdf3d18f10673c1a7554787adc
qnrjs42 <qnrjs42@naver.com>
2022-09-15 22:40:21 +0900

    #11-0. Product Model

--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -5,7 +5,7 @@ import withApiSession from '@libs/server/withSession';
 import withHandler, { ResponseType } from '@libs/server/withHandler';
 
 const meApi = async (req: NextApiRequest, res: NextApiResponse<ResponseType>) => {
-  console.log(req.session.user);
+  console.log('sessionUser: ', req.session.user);
 
   const profile = await client.user.findUnique({
     where: { id: req.session.user?.id },
