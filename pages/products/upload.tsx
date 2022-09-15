54cef855712fcbfefbd4804bdb33f21690907b5d
qnrjs42 <qnrjs42@naver.com>
2022-09-15 23:09:56 +0900

    #11-2. Upload API

--- pages/products/upload.tsx
+++ pages/products/upload.tsx
@@ -3,7 +3,10 @@ import Input from '@components/input';
 import Layout from '@components/layout';
 import TextArea from '@components/textarea';
 import useMutation from '@libs/client/useMutation';
+import { Product } from '@prisma/client';
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 
 interface UploadProductForm {
@@ -12,64 +15,75 @@ interface UploadProductForm {
   description: string;
 }
 
+interface UploadProductMutation {
+  ok: boolean;
+  product: Product;
+}
+
 const Upload: NextPage = () => {
+  const router = useRouter();
   const { register, handleSubmit } = useForm<UploadProductForm>();
-  const [uploadProduct, { loading, data }] = useMutation('/api/products');
+  const [uploadProduct, { loading, data }] = useMutation<UploadProductMutation>('/api/products');
+
+  useEffect(() => {
+    if (data?.ok) {
+      router.push(`/products/${data.product.id}`);
+    }
+  }, [data, router]);
 
   const onValid = (data: UploadProductForm) => {
-    if (!loading) return;
+    console.log(data, loading);
+    if (loading) return;
 
     uploadProduct(data);
   };
 
   return (
-    <Layout>
+    <Layout canGoBack title='Upload Product'>
       <form className='p-4 space-y-4' onSubmit={handleSubmit(onValid)}>
-        <div className='px-4 space-y-5 py-10'>
-          <div>
-            <label className='w-full text-gray-600 cursor-pointer hover:border-orange-500 hover:text-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md'>
-              <svg
-                className='h-12 w-12'
-                stroke='currentColor'
-                fill='none'
-                viewBox='0 0 48 48'
-                aria-hidden='true'
-              >
-                <path
-                  d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
-                  strokeWidth={2}
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                />
-              </svg>
+        <div>
+          <label className='w-full text-gray-600 cursor-pointer hover:border-orange-500 hover:text-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md'>
+            <svg
+              className='h-12 w-12'
+              stroke='currentColor'
+              fill='none'
+              viewBox='0 0 48 48'
+              aria-hidden='true'
+            >
+              <path
+                d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
+                strokeWidth={2}
+                strokeLinecap='round'
+                strokeLinejoin='round'
+              />
+            </svg>
 
-              <input className='hidden' type='file' />
-            </label>
-          </div>
-          <Input
-            register={register('name', { required: true })}
-            required
-            label='Name'
-            name='name'
-            type='text'
-          />
-          <Input
-            register={register('price', { required: true })}
-            required
-            label='Price'
-            name='price'
-            type='text'
-            kind='price'
-          />
-          <TextArea
-            register={register('description', { required: true })}
-            required
-            label='Description'
-            name='description'
-            type='text'
-          />
-          <Button text={loading ? 'Loading...' : 'Upload item'} />
+            <input className='hidden' type='file' />
+          </label>
         </div>
+        <Input
+          register={register('name', { required: true })}
+          required
+          label='Name'
+          name='name'
+          type='text'
+        />
+        <Input
+          register={register('price', { required: true })}
+          required
+          label='Price'
+          name='price'
+          type='text'
+          kind='price'
+        />
+        <TextArea
+          register={register('description', { required: true })}
+          required
+          label='Description'
+          name='description'
+          type='text'
+        />
+        <Button text={loading ? 'Loading...' : 'Upload item'} />
       </form>
     </Layout>
   );
