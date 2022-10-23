af43143f783b0cf2262c35aecfdb17fd2d97edcd
qnrjs42 <qnrjs42@naver.com>
2022-10-23 21:42:32 +0900

    #11-9. Unbound Mutations

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,4 +1,4 @@
-import useSWR from 'swr';
+import useSWR, { useSWRConfig } from 'swr';
 import Link from 'next/link';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
@@ -7,6 +7,7 @@ import Layout from '@components/layout';
 import { Product, User } from '@prisma/client';
 import useMutation from '@libs/client/useMutation';
 import { cls } from '@libs/client/utils';
+import useUser from '@libs/client/useUser';
 
 interface ProductWithUser extends Product {
   user: User;
@@ -20,10 +21,11 @@ interface ItemDetailResponse {
 }
 
 const ItemDetail: NextPage = () => {
+  const { user, isLoading } = useUser();
   const router = useRouter();
-  console.log(router.query);
+  const { mutate } = useSWRConfig();
 
-  const { data, mutate } = useSWR<ItemDetailResponse>(
+  const { data, mutate: boundMutate } = useSWR<ItemDetailResponse>(
     router.query.id ? `/api/products/${router.query.id}` : null,
   );
 
@@ -32,7 +34,14 @@ const ItemDetail: NextPage = () => {
   const onFavClick = () => {
     if (!data) return;
 
-    mutate({ ...data, isLiked: !data.isLiked }, false);
+    boundMutate(prev => prev && { ...prev, isLiked: !prev.isLiked }, false);
+    // mutate(
+    //   '/api/users/me',
+    //   (prev: any) => ({
+    //     ok: !prev.ok,
+    //   }),
+    //   false,
+    // );
     toggleFav({});
   };
 
